d5162ad85e34e82a8148c22220c326bff801e66f
vmediavi <victoramalv@gmail.com>
2025-05-26 04:22:49 +0200

    nav bar

--- src/main/java/com/example/eventstream/ViewController.java
+++ src/main/java/com/example/eventstream/ViewController.java
@@ -6,12 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String home() {
+        return "layout";
     }
 
     @GetMapping("/filter")
     public String filter() {
         return "filter";
+    }
+
+    @GetMapping("/edit")
+    public String edit() {
+        return "edit";
+    }
+
+    @GetMapping("/landing")
+    public String landing() {
+        return "landing";
+    }
+
+    @GetMapping("/live")
+    public String live() {
+        return "live";
     }
 }
